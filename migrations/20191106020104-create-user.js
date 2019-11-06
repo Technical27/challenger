364801211c0a811259e6f762393864a80ee93d4c
Aamaruvi Yogamani <38222826+Technical27@users.noreply.github.com>
2019-11-06 07:11:46 -0500

    created database

---
+++ migrations/20191106020104-create-user.js
@@ -0,0 +1,24 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      xp: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: queryInterface => queryInterface.dropTable('Users')
+};
---
+++ models/user.js
@@ -0,0 +1,11 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true
+    },
+    xp: DataTypes.INTEGER
+  }, {});
+
+  return User;
+};
